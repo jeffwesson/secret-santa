fc3a4dfd4051489142751257180fb33b1d4f8f51
jeffwesson <jeff@jeffwesson.com>
2017-11-23 04:19:03 -0800

    test

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(stylus.middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res, next) {
 	res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
