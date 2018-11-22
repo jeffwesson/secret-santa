bbb506e4d712a7179d0dc14eff5aa2cc1decb031
jeffwesson <jeff@jeffwesson.com>
2018-11-22 00:21:04 -0800

    use comma-last

--- models/Pair/schema.js
+++ models/Pair/schema.js
@@ -1,6 +1,6 @@
 const { Schema } = require('mongoose');
 
 module.exports = new Schema({
-	pair: [Schema.Types.ObjectId]
-	, timestamp: {type: Date, default: Date.now}
+	pair: [Schema.Types.ObjectId],
+	timestamp: { type: Date, default: Date.now }
 });
